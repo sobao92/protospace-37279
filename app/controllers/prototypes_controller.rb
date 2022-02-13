b0354ec7fc385c45eca08eac7351049f54ec6a9b
sobao92 <2mn69o0@gmail.com>
2022-02-13 14:56:51 +0900

    プロトタイプ編集ページで入力済みの項目(画像以外)は消えないように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,8 +32,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
